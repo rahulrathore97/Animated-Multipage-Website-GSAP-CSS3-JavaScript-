6382b78db495dea2f4a4d3af7d52a99597859481
RAHUL RATHORE <rahul97work@gmail.com>
2025-10-10 12:34:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -52,20 +52,20 @@ gsap.from("#about-us img, #about-us-in" ,{
          scrub:1
      }
 })
-gsap.from(".card" ,{
-    scale:0.8,
-    opacity:0,     
-    duration:1,
-    stagger:0.1,
-    scrollTrigger:{
-     trigger:".card",
-     scroller:"body",
-        //  markers:true,
-         start:"top 70%",
-         end:"top 65%",
-         scrub:1
-     }
-})
+gsap.from(".card", {
+  scale: 0.8,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.1,
+  immediateRender: false,
+  scrollTrigger: {
+    trigger: ".card",
+    scroller: "body",
+    start: "top 80%",
+    toggleActions: "play none none reverse" // ✅ instead of scrub
+  }
+});
+
 
 gsap.from("#colon1", {
     y:-70,
@@ -147,6 +147,10 @@ document.querySelectorAll("#footer div").forEach((elem) => {
       ease: "power2.in",
     });
   });
+});
+
+window.addEventListener("load", () => {
+  ScrollTrigger.refresh();
 });
 
 
